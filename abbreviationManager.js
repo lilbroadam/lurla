b5d5cf92ccf2415b81d6d3a1d2eea15045279c04
lilbroadam <lilbroadam@gmail.com>
2020-12-20 11:26:28 -0600

    Made getAbbreviationDict() async

--- abbreviationManager.js
+++ abbreviationManager.js
@@ -1,49 +1,28 @@
 const ABBREVIATION_DICTIONARY = "abbreviationDictionary";
 
-function addAbbreviationUrl(abbrUrl, redirectUrl) {
-  var abbrDict = getAbbreviationDict();
+async function addAbbreviationUrl(abbrUrl, redirectUrl) {
+  var abbrDict = await getAbbreviationDict();
+
+  console.log(abbrDict);
+
 }
 
-// Get the abbreviation dictionary from Chrome storage
-// TODO add setting to enable storage sync
 function getAbbreviationDict() {
   var abbrDict = {}; 
 
-  console.log('1')
-  chrome.storage.local.get([ABBREVIATION_DICTIONARY], function(result) {
-    console.log('2')
-    if (result) {
-      // console.log(result)
-      // console.log(typeof result)
-      // console.log(result.ABBREVIATION_DICTIONARY)
-      // console.log(typeof result.ABBREVIATION_DICTIONARY) // undefined
-      // console.log(result.abbreviationDictionary)
-      // console.log(typeof result.abbreviationDictionary) // object
-      // console.log(result[ABBREVIATION_DICTIONARY])
-      // console.log(typeof result[ABBREVIATION_DICTIONARY]) // object
-      // return result.ABBREVIATION_DICTIONARY;
-
-      console.log('3')
-      console.log(result[ABBREVIATION_DICTIONARY])
-      console.log(typeof result[ABBREVIATION_DICTIONARY]) // object
-
-      // abbrDict = result[ABBREVIATION_DICTIONARY];
-
-      for (var property in result[ABBREVIATION_DICTIONARY]) {
-        abbrDict[property] = result[ABBREVIATION_DICTIONARY][property];
+  var promise = new Promise(function(resolve, reject) {
+    chrome.storage.local.get([ABBREVIATION_DICTIONARY], function(result) {
+      if (result) {
+        abbrDict = result[ABBREVIATION_DICTIONARY];
+      } else {
+        abbrDict = {}; // No dictionary found, make one
       }
 
-    } else {
-      console.log('returning empty')
-      abbrDict = {};
-    }
+      resolve(abbrDict);
+    });
   });
 
-  console.log('4')
-  console.log(abbrDict)
-  console.log(typeof abbrDict) // object
-
-  return abbrDict;
+  return promise;
 }
 
 // function addAbbreviationUrl(abbrUrl, redirectUrl) {
@@ -65,19 +44,4 @@ function getAbbreviationDict() {
 //     document.getElementById('debug3').innerText
 //       = chrome.storage.local.get(function(result){console.log(result.ABBREVIATION_DICTIONARY)});
 //   });
-// }
-//
-// function getAbbreviationDict() {
-//   var abbrDict;
-//   // chrome.storage.local.get(ABBREVIATION_DICTIONARY, function(result) {
-//   chrome.storage.local.get(function(result) {
-//     // TODO: error handling
-//     if(result) {
-//       abbrDict = result.ABBREVIATION_DICTIONARY;
-//     } else {
-//       abbrDict = {};
-//     }
-//   });
-//
-//   return abbrDict;
 // }
