459ed47c6924be49be76311b9df6ddabfdb3a1f7
lilbroadam <lilbroadam@gmail.com>
2020-12-20 09:32:51 -0600

    Wrote getAbbreviationDict() but need to async it now

--- abbreviationManager.js
+++ abbreviationManager.js
@@ -1,5 +1,51 @@
 const ABBREVIATION_DICTIONARY = "abbreviationDictionary";
 
+function addAbbreviationUrl(abbrUrl, redirectUrl) {
+  var abbrDict = getAbbreviationDict();
+}
+
+// Get the abbreviation dictionary from Chrome storage
+// TODO add setting to enable storage sync
+function getAbbreviationDict() {
+  var abbrDict = {}; 
+
+  console.log('1')
+  chrome.storage.local.get([ABBREVIATION_DICTIONARY], function(result) {
+    console.log('2')
+    if (result) {
+      // console.log(result)
+      // console.log(typeof result)
+      // console.log(result.ABBREVIATION_DICTIONARY)
+      // console.log(typeof result.ABBREVIATION_DICTIONARY) // undefined
+      // console.log(result.abbreviationDictionary)
+      // console.log(typeof result.abbreviationDictionary) // object
+      // console.log(result[ABBREVIATION_DICTIONARY])
+      // console.log(typeof result[ABBREVIATION_DICTIONARY]) // object
+      // return result.ABBREVIATION_DICTIONARY;
+
+      console.log('3')
+      console.log(result[ABBREVIATION_DICTIONARY])
+      console.log(typeof result[ABBREVIATION_DICTIONARY]) // object
+
+      // abbrDict = result[ABBREVIATION_DICTIONARY];
+
+      for (var property in result[ABBREVIATION_DICTIONARY]) {
+        abbrDict[property] = result[ABBREVIATION_DICTIONARY][property];
+      }
+
+    } else {
+      console.log('returning empty')
+      abbrDict = {};
+    }
+  });
+
+  console.log('4')
+  console.log(abbrDict)
+  console.log(typeof abbrDict) // object
+
+  return abbrDict;
+}
+
 // function addAbbreviationUrl(abbrUrl, redirectUrl) {
 //   var abbrDict = getAbbreviationDict();
 //   // if(abbrDict == null) {
